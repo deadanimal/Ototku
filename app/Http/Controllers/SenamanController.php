a129a30458716255903c16e18d2c37a9aeba7068
Afeez Aziz <afeezaziz@gmail.com>
2022-10-13 14:41:27 +0800

    jin

---
+++ app/Http/Controllers/SenamanController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DataTables;
+use DateTime;
+use Carbon\Carbon;
+
+
+use Illuminate\Http\Request;
+use App\Models\Senaman;
+
+class SenamanController extends Controller
+{
+    
+    public function senarai_senaman(Request $request) {}
+
+    public function satu_senaman(Request $request) {}
+    
+    public function cipta_senaman(Request $request) {}
+
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DataTables;
+use DateTime;
+use Carbon\Carbon;
+
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    
+    public function senarai_user(Request $request) {}
+
+    public function satu_user(Request $request) {}
+    
+    public function cipta_user(Request $request) {}
+
+}
